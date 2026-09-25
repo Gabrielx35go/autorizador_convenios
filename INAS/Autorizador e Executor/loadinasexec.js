9e3b27c2cf78f26a1755e157c3a1ad4e05053ac5
Gabrielx35go <54038315+Gabrielx35go@users.noreply.github.com>
2026-09-25 11:43:36 -0300

    Delete INAS/Autorizador e Executor/loadinasexec.js

--- INAS/Autorizador e Executor/loadinasexec.js
+++
@@ -1,3 +0,0 @@
-fetch("https://raw.githubusercontent.com/Gabrielx35go/autorizador_convenios/refs/heads/main/INAS/Autorizador%20e%20Executor/executor_inas.js")
-  .then(r => r.text())
-  .then(eval);
